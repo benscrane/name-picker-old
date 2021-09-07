69a8b02d9d05c3e9b499977c72d0e674ba701c3f
Ben Crane <bencrane@protonmail.com>
2021-09-07 23:09:23 +0000

    add backend routes

--- frontend/src/views/admin/LunchMenuAdmin.svelte
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -1,5 +1,26 @@
 <script>
+  import { onMount } from 'svelte';
+  import axios from 'axios';
   import { user } from '../../stores';
+  import { navigateTo } from 'svelte-router-spa';
+
+  let lunchWeekList = [];
+
+  onMount(async () => {
+    try {
+      const response = await axios.get(
+        'https://3000-olive-yak-8ifswvvp.ws-us15.gitpod.io/api/lunch-week'
+      );
+      lunchWeekList = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  const openLunchWeekDetails = (lunchWeek) => {
+    const route = `/admin/manage-menus/week-details/${lunchWeek.lunchWeekId}`;
+    navigateTo(route);
+  };
 </script>
 
 <div>
@@ -16,4 +37,22 @@
       </li>
     </ul>
   </nav>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Week Of</th>
+        <th>Published</th>
+      </tr>
+    </thead>
+    {#each lunchWeekList as lunchWeek}
+      <tr
+        on:click={openLunchWeekDetails(lunchWeek)}
+        class="has-text-link"
+        style="cursor: pointer"
+      >
+        <td>{lunchWeek.weekOf}</td>
+        <td>{lunchWeek.isPublished}</td>
+      </tr>
+    {/each}
+  </table>
 </div>
